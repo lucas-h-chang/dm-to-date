9caeb5c7b7ef8acc0139982f5a44d26f4686e58b
lucas-h-chang <lucaschang2006@gmail.com>
2025-09-01 11:00:02 -0700

    Update vite.config.ts
    base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/dm-to-date/',
   server: {
     host: "::",
     port: 8080,
